5189bef93b054a6c67858e21e4e6ee6b1e175bcf
zou <675350014@qq.com>
2023-09-28 21:21:24 +0800

    优化组件展示样式

--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
+  <DemoPage>
+    <DemoBlock title="基本用法">
 
-    <vux-divider>
-      基本用法
-    </vux-divider>
-
-    <vux-space>
-
+    </DemoBlock>
+    <vux-space size="12">
       <vux-button size="small" @click="top">
         顶部弹出
       </vux-button>
@@ -20,18 +17,22 @@
         右侧弹出
       </vux-button>
     </vux-space>
+    <DemoBlock title="基本用法">
+
+    </DemoBlock>
     <vux-space>
-      <vux-button size="small" @click="top">
+      <vux-button size="small" @click="bottomShow1=true">
         圆角弹窗
       </vux-button>
     </vux-space>
 
-    <vux-popup :popup-style="{width:'100%',height:'30%'}" :show.sync="topShow" closeable position="top">
+    <vux-popup :popup-style="{height:'30%'}" :show.sync="topShow" closeable position="top">
     </vux-popup>
-    <vux-popup :popup-style="{width:'80%',height:'100%'}" :show.sync="leftShow" position="left"></vux-popup>
-    <vux-popup :popup-style="{width:'80%',height:'100%'}" :show.sync="rightShow" position="right"></vux-popup>
-    <vux-popup :popup-style="{width:'100%',height:'30%'}" :show.sync="bottomShow" position="bottom"></vux-popup>
-  </div>
+    <vux-popup :popup-style="{width:'80%'}" :show.sync="leftShow" position="left"></vux-popup>
+    <vux-popup :popup-style="{width:'80%'}" :show.sync="rightShow" position="right"></vux-popup>
+    <vux-popup :popup-style="{height:'30%'}" :show.sync="bottomShow" position="bottom"></vux-popup>
+    <vux-popup :popup-style="{width:'100%',height:'30%'}" :show.sync="bottomShow1" position="bottom" round></vux-popup>
+  </DemoPage>
 </template>
 
 <script>
@@ -39,10 +40,12 @@ import VuxDivider from "@/packages/divider/src/index.vue";
 import VuxSpace from "@/packages/space/src/index.vue";
 import VuxPopup from "@/packages/popup/index.vue";
 import VuxButton from "@/packages/button/src/index.vue";
+import DemoBlock from "@/components/DemoBlock.vue";
+import DemoPage from "@/components/DemoPage.vue";
 
 export default {
   name: "Popup",
-  components: {VuxButton, VuxPopup, VuxSpace, VuxDivider},
+  components: {DemoPage, DemoBlock, VuxButton, VuxPopup, VuxSpace, VuxDivider},
   data() {
     return {
 
@@ -50,6 +53,7 @@ export default {
       leftShow: false,
       rightShow: false,
       bottomShow: false,
+      bottomShow1: false,
     }
   },
 
